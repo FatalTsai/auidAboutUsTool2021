f3851b0a4d7dab23c1292d6c2196a7871d7e486a
cheney <cheney.tsai@jet-opto.com.tw>
2021-01-20 13:59:32 +0800

    second

--- replaceHref.js
+++ replaceHref.js
@@ -16,12 +16,19 @@ fs.readdir(testFolder, (err, files) => {
 
 
     let tmp=''
-    // tmp = raw_html.match(/<a href="http:\/\/www\.ec\.europa\.eu\/consumers\/odr"/gs) //delete hyper link
-    // raw_html=raw_html.replace(/<a href="http:\/\/www\.ec\.europa\.eu\/consumers\/odr" /gs,'<a href')
+    tmp = raw_html.match(/<a href="http:\/\/www\.ec\.europa\.eu\/consumers\/odr"/gs) //delete hyper link 
+    raw_html=raw_html.replace(/<a href="http:\/\/www\.ec\.europa\.eu\/consumers\/odr" /gs,'<a href') 
 
-    tmp = raw_html.match(/\<img src="(.*?)hspace="12"\/>/gs) //delete img tag
+    tmp = raw_html.match(/\<img src="(.*?)hspace="12"\/>/gs) //delete img tag https://regex101.com/r/iN6pX6/271
     raw_html=raw_html.replace(/\<img src="(.*?)hspace="12"\/>/gs,'\n')
 
+    tmp = raw_html.match(/\<font size="3"/gs) //delete img tag https://regex101.com/r/iN6pX6/271
+    raw_html=raw_html.replace(/\<font size="3"/gs,'<font size="4"')
+    raw_html=raw_html.replace(/\<font size="5"/gs,'<font size="6" class="aboutusTitle"')
+
+    // tmp = raw_html.match(/\<font size="3"/gs) //delete internal tag https://regex101.com/r/iN6pX6/272
+    raw_html=raw_html.replace(/\<span id="Frame1"(.*?)<\/span>/gs,'\n')
+    
     console.log(tmp)
 
 
